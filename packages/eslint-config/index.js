58a341af4016188ae63578f6ab9cc9c75f0944c0
ykob <info@tplh.net>
2025-07-21 23:40:47 +0900

    added prettier and  eslint.

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx}"],
+    plugins: {
+      react: reactPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  }
+);
